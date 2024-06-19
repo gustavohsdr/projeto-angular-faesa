881f4571846166f358c1c3d303cec3671734a183
Gustavo Henrique <ghsdare@gmail.com>
2024-06-19 09:41:55 -0300

    Initial commit

---
+++ src/app/components/newsletter-form/newsletter-form.component.spec.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'newsletter-form',
+  standalone: true,
+  imports: [BtnPrimaryComponent, ReactiveFormsModule],
+  templateUrl: './newsletter-form.component.html',
+  styleUrls: ['./newsletter-form.component.scss'],
+})
+export class NewsletterFormComponent {
+  newsletterForm!: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder) {
+    this.newsletterForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onFocus(event: FocusEvent): void {
+    const inputElement = event.target as HTMLInputElement;
+    inputElement.classList.add('focused');
+  }
+
+  onBlur(event: FocusEvent): void {
+    const inputElement = event.target as HTMLInputElement;
+    inputElement.classList.remove('focused');
+    if (inputElement.value) {
+      inputElement.classList.add('filled');
+    } else {
+      inputElement.classList.remove('filled');
+    }
+  }
+
+  onSubmit(): void {
+    if (this.newsletterForm.valid) {
+      this.loading = true;
+      // Simulate an HTTP request
+      setTimeout(() => {
+        console.log('Form Submitted', this.newsletterForm.value);
+        this.loading = false; // Reset loading state after submission
+      }, 2000);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
